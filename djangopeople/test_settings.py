46090bdbdb1de696c9b8a346498c0b305102c18d
Bruno Renié <buburno@gmail.com>
2012-11-22 22:49:16 +0100

    Fixed the tests settings

--- djangopeople/test_settings.py
+++ djangopeople/test_settings.py
@@ -1,20 +1,10 @@
 import warnings
 warnings.simplefilter('always')
 
-from default_settings import *  # noqa
-
-SECRET_KEY = 'test secret key'
+from .settings import *  # noqa
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 BASE_PATH = os.path.join(OUR_ROOT, os.pardir)
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangopeople',
-        'USER': 'postgres',
-    },
-}
 
 LOGGING['loggers']['raven']['handlers'] = []
 LOGGING['loggers']['sentry.errors']['handlers'] = []
