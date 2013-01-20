d588a2c68eac275874e19a69006e5451db70c542
Bruno Renié <buburno@gmail.com>
2013-01-20 21:20:36 +0100

    Speed up the test suite by disabling strong password hashing

--- djangopeople/test_settings.py
+++ djangopeople/test_settings.py
@@ -6,5 +6,9 @@ from .settings import *  # noqa
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 BASE_PATH = os.path.join(OUR_ROOT, os.pardir)
 
+PASSWORD_HASHERS = [
+    'tests.hashers.NotHashingHasher',
+]
+
 LOGGING['loggers']['raven']['handlers'] = []
 LOGGING['loggers']['sentry.errors']['handlers'] = []
