98b2e96063131e57ca8c7d015ff78cc9aa26833b
Yohan Boniface <yb@enix.org>
2013-05-20 18:19:51 +0200

    First shot in js "leafletization" (cf #45)

--- djangopeople/djangopeople/static/djangopeople/js/maps.js
+++ djangopeople/djangopeople/static/djangopeople/js/maps.js
@@ -1,68 +1,157 @@
-function getPersonPopupContent(person) {
-    var STATIC_URL = $('body').data('static-url');
-    var lat = person[0];
-    var lon = person[1];
-    var name = person[2];
-    var username = person[3];
-    var location_description = person[4];
-    var photo = person[5];
-    var iso_code = person[6];
-    var html =  '<ul class="detailsList">' +
-        '<li>' +
-        '<img src="' + photo + '" alt="' + name + '" class="main">' +
-        '<h3><a href="/' + username + '/">' + name + '</a></h3>' +
-        '<p class="meta"><a href="/' + iso_code + '/" class="nobg">' +
-        '<img src="' + STATIC_URL + 'djangopeople/img/flags/' + iso_code + '.gif"></a> ' +
-        location_description + '</p>' +
-        '<p class="meta"><a href="#" onclick="zoomOn(' + lat + ', ' + lon + '); return false;">Zoom to point</a></p>' +
-        '</li>';
-    return html;
-}
+STATIC_URL = $('body').data('static-url');
 
-function zoomOn(lat, lon) {
-    MAP.setCenter(new L.LatLng(lat, lon));
-    MAP.setZoom(12);
-}
+var ShrinkMapControl = L.Control.extend({
+    options: {
+        position: 'bottomleft'
+    },
 
-/* Plots people on the maps and adds an info window to it
- * which becomes visible when you click the marker
- */
-function plotPeopleOnMap(people, map) {
-    var bounds = new L.LatLngBounds();
-    $.each(people, function(index, person) {
-        var marker = getPersonMarker(person);
-        bounds.extend(marker.getLatLng());
-        marker.addTo(map);
-    });
-    map.fitBounds(bounds);
-    return bounds;
-}
+    onAdd: function (map) {
+        // create the control container with a particular class name
+        var container = L.DomUtil.create('div', 'shrinkControl');
+        container.innerHTML = 'Shrink map';
+        L.DomEvent.addListener(container, 'click', this.onClick, this);
 
-// Creates a Marker object for a person
-function getPersonMarker(person) {
-    var lat = person[0];
-    var lon = person[1];
-    var point = new L.LatLng(lat, lon);
-    // custom marker options removed for now
-    var marker = new L.Marker(point, {
-        icon: greenIconImage(),
-    });
-    var info = getPersonPopupContent(person);
-    marker.bindPopup(info)
+        return container;
+    },
 
-    return marker;
-}
+    onClick: function (e) {
+        this._map.shrink();
+        L.DomEvent.stop(e);
+    }
+});
 
-function greenIconImage() {
-    var STATIC_URL = $('body').data('static-url');
-    var greenIcon = L.icon({
+
+var DJIcon = L.Icon.extend({
+    options: {
         iconUrl: STATIC_URL + 'djangopeople/img/green-bubble.png',
         shadowUrl: STATIC_URL + 'djangopeople/img/green-bubble-shadow.png',
-        iconSize:     [32, 32], // size of the icon
-        shadowSize:   [32, 32], // size of the shadow
-        iconAnchor:   [16, 32], // point of the icon which will correspond to marker's location
+        iconSize:     [32, 32],  // size of the icon
+        shadowSize:   [32, 32],  // size of the shadow
+        iconAnchor:   [16, 32],  // point of the icon which will correspond to marker's location
         shadowAnchor: [10, 32],  // the same for the shadow
-        popupAnchor:  [0, -32] // point from which the popup should open relative to the iconAnchor
-    });
-    return greenIcon;
-}
+        popupAnchor:  [0, -32]  // point from which the popup should open relative to the iconAnchor
+    }
+});
+
+var DJMarker = L.Marker.extend({
+
+    options: {
+        icon: new DJIcon()
+    },
+
+    initialize: function (person) {
+        lat = person[0];
+        lng = person[1];
+        var latlng = new L.LatLng(lat, lng);
+        L.Marker.prototype.initialize.call(this, latlng);
+        this.person = person;
+        this.name = person[2];
+        this.username = person[3];
+        this.location = this.person[4];
+        this.photo = person[5];
+        this.iso_code = person[6];
+        this.initPopup();
+    },
+
+    initPopup: function () {
+        var container = L.DomUtil.create('ul', 'detailsList'),
+            li = L.DomUtil.create('li', '', container),
+            img = L.DomUtil.create('img', '', li);
+        img.src = this.photo;
+        var h3 = L.DomUtil.create('h3', '', li),
+            a = L.DomUtil.create('a', '', h3);
+        a.href = '/' + this.username + '/';
+        a.innerHTML = this.name;
+        var p = L.DomUtil.create('p', 'meta', li);
+        a = L.DomUtil.create('a', 'nobg', p);
+        a.href = '/' + this.iso_code + '/';
+        img = L.DomUtil.create('img', '', a);
+        img.src = STATIC_URL + 'djangopeople/img/flags/' + this.iso_code + '.gif';
+        a.innerHTML = a.innerHTML + " " + this.location;
+        p = L.DomUtil.create('p', 'meta', li);
+        a = L.DomUtil.create('a', '', p);
+        a.innerHTML = "Zoom to point";
+        L.DomEvent.on(a, "click", L.DomEvent.stop)
+                  .on(a, "click", this.bringToCenter, this);
+        this.bindPopup(container);
+    },
+
+    bringToCenter: function (e) {
+        var latlng;
+        if (e && e.latlng) {
+            latlng = e.latlng;
+        }
+        else {
+            latlng = this.getLatLng();
+        }
+        this._map.panTo(latlng);
+        this._map.setZoom(8);
+    }
+
+});
+
+var DJMap = L.Map.extend({
+
+    /* Plots people on the maps and adds an info window to it
+     * which becomes visible when you click the marker
+     */
+    plotPeople: function (people) {
+        var bounds = new L.LatLngBounds(),
+            map = this;
+        $.each(people, function(index, person) {
+            var marker = new DJMarker(person);
+            bounds.extend(marker.getLatLng());
+            marker.addTo(map);
+        });
+        this.fitBounds(bounds);
+        return bounds;
+    },
+
+    unShrink: function () {
+        var map = this;
+        // Unbind event so user can actually interact with map
+        map.off('click', this.unShrink);
+
+        $('#map').css({'cursor': ''}).attr('title', '');
+        $('#map').animate({
+            height: '25em',
+            opacity: 1.0
+        }, 500, 'swing', function() {
+            map.invalidateSize();
+            if (map.shrinkControl) {
+                map.shrinkControl.addTo(map);
+            }
+        });
+    },
+
+    shrink: function (latlng) {
+        latlng = latlng || this.getCenter();
+        this.panTo(latlng);
+        if (this.shrinkControl) {
+            this.shrinkControl.removeFrom(this);
+        }
+        var map = this;
+
+        $('#map').css({'cursor': 'pointer'}).attr(
+            'title', 'Activate larger map'
+        );
+        $('#map').animate({
+            height: '7em',
+            opacity: 0.6
+        }, 500, 'swing', function() {
+            map.invalidateSize();
+        });
+
+        /* Map enlarges and becomes active when you click on it */
+        this.on('click', this.unShrink);
+    },
+
+    shrinkable: function (latlng) {
+        this.shrinkControl = new ShrinkMapControl().addTo(this);
+
+        // Marker for the current profile, not clickable
+        new L.Marker(latlng, {icon: new DJIcon()}).addTo(this);
+        this.shrink(latlng);
+    }
+
+});
