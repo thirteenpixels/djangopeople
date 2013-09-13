80815dadbb8254a4ffd4b0382a69bf2a3059219d
Bill Evans <bill@squarefactor.com>
2013-09-13 13:54:47 -0400

    adding postmark for emails

--- djangopeople/settings.py
+++ djangopeople/settings.py
@@ -185,13 +185,13 @@ if 'CANONICAL_HOSTNAME' in os.environ:
     CANONICAL_HOSTNAME = os.environ['CANONICAL_HOSTNAME']
     ALLOWED_HOSTS = [CANONICAL_HOSTNAME]
 
-SERVER_EMAIL = DEFAULT_FROM_EMAIL = os.environ['FROM_EMAIL']
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'postmark.django_backend.EmailBackend'
     POSTMARK_API_KEY = os.environ['POSTMARK_API_KEY']
     POSTMARK_SENDER = os.environ['POSTMARK_SENDER']
+    SERVER_EMAIL = DEFAULT_FROM_EMAIL = POSTMARK_SENDER
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_BUCKET_NAME']
